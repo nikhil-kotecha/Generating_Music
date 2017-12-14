3f4a71b62b72d69e1e00af2d9cb115cfc3137b2b
paulyoung <paulyoung48@gmail.com>
2017-12-14 01:28:53 -0500

    More Code Cleanup - cleaned up the LSTM-time and note axis segments.  Found several errors due to mismanagement of reshaping. - added ability to specifiy arbitrary number and size of layers for both LSTM-time and note axis segments.

--- MyFunctions.py
+++ MyFunctions.py
@@ -4,50 +4,42 @@ from tensorflow.python.ops import math_ops
 from tensorflow.contrib.rnn import BasicLSTMCell
 from tensorflow.contrib.rnn import LSTMStateTuple
 
-#def Input_Kernel_numpy(input_data, Midi_low=24, Midi_high=102):
-"""
-Arguments:
-Note_State_Batch: numpy size = [batch_size x num_notes x num_timesteps]
-Midi_low: integer
-Midi_high: integer
-    
-With numpy, 
-"""    
-#return Note_State_Expand, prev_input
 
 
-def Input_Kernel(input_data, Midi_low, Midi_high, time_init=0):
+def Input_Kernel(input_data, Midi_low, Midi_high, time_init):
     """
     Arguments:
         input_data: size = [batch_size x num_notes x num_timesteps x 2] 
+            (the input data represents that at the previous timestep of what we are trying to predict)
         Midi_low: integer
         Midi_high: integer
-    the input data represents that at the previous timestep of what we are trying to predict
+        time_init: integer representing where the 'beat' component begins for the batch.
+    Returns:
+        Note_State_Expand: size = [batch_size x num_notes x num_timesteps x 80]
     """    
 
     
-    # batch_size and num_timesteps are variable length
+    # Capture input_data dimensions (batch_size and num_timesteps are variable length)
     batch_size = tf.shape(input_data)[0]
     num_notes = input_data.get_shape()[1].value
     num_timesteps = tf.shape(input_data)[2]
     
     
     
-    # MIDI note number
+    # MIDI note number (only a function of the note index)
     Midi_indices = tf.squeeze(tf.range(start=Midi_low, limit = Midi_high+1, delta=1))
     x_Midi = tf.ones((batch_size, num_timesteps, 1, num_notes))*tf.cast(Midi_indices, dtype=tf.float32)
     x_Midi = tf.transpose(x_Midi, perm=[0,3,1,2]) # shape = batch_size, num_notes, num_timesteps, 1
     #print('x_Midi shape = ', x_Midi.get_shape())
 
     
-    # part_pitchclass
+    # part_pitchclass (only a function of the note index)
     Midi_pitchclasses = tf.squeeze(x_Midi % 12, axis=3)
-    #Midi_pitchclasses = tf.squeeze(x_Midi % 12, axis = 3)
     x_pitch_class = tf.one_hot(tf.cast(Midi_pitchclasses, dtype=tf.uint8), depth=12)
     #print('x_pitch_class shape = ', x_pitch_class.get_shape())
     
    
-    # part_prev_vicinity (1 time step in past)
+    # part_prev_vicinity
     input_flatten = tf.transpose(input_data, perm=[0,2,1,3])
     input_flatten = tf.reshape(input_flatten, [batch_size*num_timesteps, num_notes, 2]) # channel for play and channel for articulate
     input_flatten_p = tf.slice(input_flatten, [0,0,0],size=[-1, -1, 1])
@@ -56,7 +48,6 @@ def Input_Kernel(input_data, Midi_low, Midi_high, time_init=0):
     # reverse identity kernel
     filt_vicinity = tf.expand_dims(tf.eye(25), axis=1)
 
-   
     #1D convolutional filter for each play and articulate arrays 
     vicinity_p = tf.nn.conv1d(input_flatten_p, filt_vicinity, stride=1, padding='SAME')
     vicinity_a = tf.nn.conv1d(input_flatten_a, filt_vicinity, stride=1, padding='SAME')    
@@ -72,7 +63,7 @@ def Input_Kernel(input_data, Midi_low, Midi_high, time_init=0):
     #print('x_prev vicinity shape = ', x_vicinity.get_shape())  
    
 
-    #part_prev_context (1 time step in past)
+    #part_prev_context
     input_flatten_p_bool = tf.minimum(input_flatten_p,1) 
     # 1 if note is played, 0 if not played.  Don't care about articulation
     
@@ -87,7 +78,7 @@ def Input_Kernel(input_data, Midi_low, Midi_high, time_init=0):
     
     
     
-    #beat - for music generation, need to keep track of 't' from batch to batch (timestep to timestep)
+    #beat (only a function of the time axis index plus the time_init value
     Time_indices = tf.range(time_init, num_timesteps + time_init)
     x_Time = tf.reshape(tf.tile(Time_indices, multiples=[batch_size*num_notes]), shape=[batch_size, num_notes, num_timesteps,1])
     x_beat = tf.cast(tf.concat([x_Time%2, x_Time//2%2, x_Time//4%2, x_Time//8%2], axis=-1), dtype=tf.float32)
@@ -101,27 +92,31 @@ def Input_Kernel(input_data, Midi_low, Midi_high, time_init=0):
     Note_State_Expand = tf.concat([x_Midi, x_pitch_class, x_vicinity, x_context, x_beat, x_zero], axis=-1)
     
     
-    return Note_State_Expand, time_init
+    return Note_State_Expand
 
 
 
 
-def LSTM_TimeWise_Training_Layer(input_data, state_in):
+def LSTM_TimeWise_Training_Layer(input_data, state_init):
     """
     Arguments:
         input_data: Tensor with size = [batch_size x num_notes x num_timesteps x input_size]
-        initial_state: Tuple of 2 Tensors each with size =  [batch_size x num_notes x num_units]
+        state_init: List of LSTMTuples([batch_size*num_notes x num_units[layer]], [batch_size*num_notes x num_units[layer]])
+        
+    Returns:
+        output: tensor with size = [batch_size*num_notes x num_timesteps x num_units_final
+        state: List of LSTMTuples([batch_size*num_notes x num_units[layer]], [batch_size*num_notes x num_units[layer]])
         
     # LSTM time-wise 
     # This section is the 'Model LSTM-TimeAxis' block and will run a number of LSTM cells over the time axis.
     # Every note and sample in the batch will be run in parallel with the same LSTM weights
-    # The input data is 'Note_State_Filt' with dimensions batch_size x num_notes x num_timesteps x input_size
-    # The output will be 'Hid_State_Final' with dimensions batch_size x num_notes x num_timesteps x num_units
+
 
     
     # Reshape the input
     # batch_size and num_notes dimensions of input are flattened to treat as single 'batch' dimension for LSTM cell
     # will be reshaped at the end of this block for the next stage
+    # state_init is already flat for convenience
   """  
     
     # batch_size and num_timesteps are variable length
@@ -130,38 +125,37 @@ def LSTM_TimeWise_Training_Layer(input_data, state_in):
     num_timesteps = tf.shape(input_data)[2]
     input_size = input_data.get_shape()[3].value
     
-    c_in, h_in = state_in
-    num_units = c_in.get_shape()[2].value
+    num_layers = len(state_init)    
     
-    
-    #Flatten input_data and state_in to allow note steps to be run in parallel as separate batches for this segment
+    # Flatten input
     input_flatten = tf.reshape(input_data, shape=[batch_size*num_notes, num_timesteps, input_size])
-    c_in_flatten = tf.reshape(c_in, shape=[batch_size*num_notes, num_units])
-    h_in_flatten = tf.reshape(h_in, shape=[batch_size*num_notes, num_units])    
-    state_in_flatten = LSTMStateTuple(c_in_flatten, h_in_flatten)
     
-    #Instantiate Time-Wise Cell
-    lstmcell_time = BasicLSTMCell(num_units=num_units, forget_bias=1.0, state_is_tuple=True,activation=math_ops.tanh, reuse=None)
+    # generate cell list of length specified by initial state
+    cell_list=[]
+    num_states=[]
+    for h in range(num_layers):
+        num_states.append(state_init[h][0].get_shape()[1].value)
+        lstm_cell = BasicLSTMCell(num_units=num_states[h], forget_bias=1.0, state_is_tuple=True,activation=math_ops.tanh, reuse=None)
+        cell_list.append(lstm_cell)
+    
+
+    #Instantiate multi layer Time-Wise Cell
+    multi_lstm_cell = tf.contrib.rnn.MultiRNNCell(cell_list, state_is_tuple=True)
 
 
     #Run through LSTM time steps and generate time-wise sequence of outputs
-    out_flat, (c_flat, h_flat) = tf.nn.dynamic_rnn(lstmcell_time, input_flatten, initial_state=state_in_flatten, dtype=tf.float32)
+    output_flat, state_out = tf.nn.dynamic_rnn(cell=multi_lstm_cell, inputs=input_flatten, initial_state=state_init, dtype=tf.float32)
 
-
-    #Unflatten the 1st 2 dimensions [Lbatch, Nnotes, num_timesteps, num_units]
-    output = tf.reshape(out_flat, shape=[batch_size, num_notes, num_timesteps, num_units])
-    h = tf.reshape(h_flat, shape=[batch_size, num_notes, num_units])    
-    c = tf.reshape(c_flat, shape=[batch_size, num_notes, num_units])     
-    
-    state_out = LSTMStateTuple(c, h)
+    output = tf.reshape(output_flat, shape=[batch_size, num_notes, num_timesteps, num_states[-1]])
     
     return output, state_out
 
 
-def LSTM_NoteWise_Layer(input_data, num_class=2):
+def LSTM_NoteWise_Layer(input_data, state_init, num_class=2):
     """
     Arguments:
-        input_data: size = [batch_size x num_notes x num_timesteps x num_hidden_units]
+        input_data: size = [batch_size x num_notes x num_timesteps x size_input]
+        state_init: List of LSTMTuples([batch_size*num_time_steps x num_units[layer]], [batch_size*num_timesteps x num_units[layer]]) 
         num_class: number of possible integer values for Note_State_Batch entries
           
     # LSTM note-wise
@@ -184,64 +178,87 @@ def LSTM_NoteWise_Layer(input_data, num_class=2):
     batch_size = tf.shape(input_data)[0]
     num_notes = input_data.get_shape()[1].value
     num_timesteps = tf.shape(input_data)[2]
-    num_units = input_data.get_shape()[3].value
+    input_size = input_data.get_shape()[3].value
+    
+    num_layers = len(state_init) 
 
     # Reshape the input
     # batch_size and num_timesteps dimensions of input are flattened to treat as single 'batch' dimension for LSTM cell
-    notewise_in = tf.reshape(input_data, [batch_size*num_timesteps, num_notes, num_units])
+    notewise_in = tf.transpose(input_data, perm=[0,2,1,3])
+    notewise_in = tf.reshape(notewise_in, shape=[batch_size*num_timesteps, num_notes, input_size])
+    
+    # generate LSTM cell list of length specified by initial state
+    cell_list=[]
+    num_states=[]
+    for h in range(num_layers):
+        num_states.append(state_init[h][0].get_shape()[1].value)
+        lstm_cell = BasicLSTMCell(num_units=num_states[h], forget_bias=1.0, state_is_tuple=True,activation=math_ops.tanh, reuse=None)
+        cell_list.append(lstm_cell)
 
-
+    #Instantiate multi layer Time-Wise Cell
+    multi_lstm_cell = tf.contrib.rnn.MultiRNNCell(cell_list, state_is_tuple=True) 
+    
+   
     # For this LSTM cell, can't use tf.nn.dynamic_rnn because samples have to be generated and fed back to for subsequent notes
     # need to feed the generated output for note 'n-1' into the generation of note 'n'
     # Will use 'for' loop and call the LSTM cell each time
 
-
-    #Instantiate Note-Wise Cell
-    lstmcell_note = BasicLSTMCell(num_units=4, forget_bias=1.0, state_is_tuple=True,activation=math_ops.tanh, reuse=None)
-
+   
 
     #Set values for initial LSTM state and sampled note.  Zero notes always played below bottom note
-    logP_state_initial = tf.zeros([batch_size*num_timesteps, 4])
-    pa_gen_initial = tf.zeros([batch_size*num_timesteps,2, 1])
+    h_state = state_init
+    note_gen_n = tf.zeros([batch_size*num_timesteps, 2])
 
-    logP_n_state = LSTMStateTuple(logP_state_initial, logP_state_initial) #(c, h)
-    pa_gen_n_unflat = pa_gen_initial
-
-    logP_out_list=[]
-    pa_gen_out_list=[]
-    #print('notewise shape = ', notewise_in.get_shape())
-    #print('pa_gen_n shape = ', pa_gen_n_unflat.get_shape())
-    
-    
-    
+    y_list=[]
+    note_gen_list=[]
+  
+   
     #Run through notes for note-wise LSTM to obtain P(va(n) | va(<n))
     for n in range(num_notes):    
         #concatenate previously sampled note play-articulate-combo with timewise output
-        p_gen = tf.squeeze(tf.slice(pa_gen_n_unflat, [0,0,0],[-1,1,-1]), axis=2)
-        a_gen = tf.squeeze(tf.slice(pa_gen_n_unflat, [0,1,0],[-1,1,-1]), axis=2)
-        cell_inputs = tf.concat([notewise_in[:,n,:], tf.cast(p_gen, dtype=tf.float32),  tf.cast(a_gen, dtype=tf.float32)], axis=1)
+        p_gen = tf.cast(tf.slice(note_gen_n, [0,0],[-1,1]), tf.float32)
+        a_gen = tf.cast(tf.slice(note_gen_n, [0,1],[-1,1]), tf.float32)
+        cell_inputs = tf.concat([notewise_in[:,n,:], p_gen, a_gen], axis=-1)
         #print('Cell inputs shape = ', cell_inputs.get_shape())
-        #print('logP_n_state shape = ', logP_n_state[0].get_shape())
         
-        # Run single note step of LSTM cell and reshape output from [batch_size*num_timesteps, 4] [batch_size*num_timesteps*2, 2]
-        logP_n_out, logP_n_state = lstmcell_note(cell_inputs, logP_n_state)       
-        logP_n_out_unflat = tf.reshape(logP_n_out, shape=[batch_size*num_timesteps*2, 2])
+        # output shape = [batch_size*num_timesteps, Nfinal] 
+        h_final_out, h_state = multi_lstm_cell(inputs=cell_inputs, state=h_state)       
+        #print('h_final_out shape = ', h_final_out.get_shape())
+        #print('h_state len = ', len(h_state))
         
-        # Sample play and articulate from logPout distribution and reshape 
-        pa_gen_n = tf.multinomial(logits=logP_n_out_unflat, num_samples=1)          
-        pa_gen_n_unflat = tf.reshape(pa_gen_n, shape=[batch_size*num_timesteps, 2, 1])
+        # Fully Connected Layer to generate 4 outputs: logP(p=0), logP(p=1), logP(a=0), logP(a=1)
+        y_n = tf.layers.dense(inputs=h_final_out, units=4, activation=None)
+        #print('y_n shape = ', y_n.get_shape())
+        
+        # Restack so that shape of [batch_size*num_timesteps, 4] becomes shape=[batch_size*num_timesteps*2, 2] for multinomial argument
+        # Reshape needs to be done by manipulating only the 1st 2 dimensions?       
+        y_n = tf.transpose(y_n, perm=[1,0])
+        y_n = tf.reshape(y_n, shape=[2, 2, batch_size*num_timesteps])
+        y_n = tf.transpose(y_n, perm=[2, 0, 1])
+        y_n = tf.reshape(y_n, shape=[batch_size*num_timesteps*2, 2])
+        
+        # Sample the 'play' and 'articulate' values from y_out log probabilities  
+        note_gen_n = tf.multinomial(logits=y_n, num_samples=1)          
+        note_gen_n = tf.squeeze(note_gen_n, axis=-1)
+        note_gen_n = tf.reshape(note_gen_n, shape=[batch_size*num_timesteps, 2])        
+        
+        # Reshape                         
+        y_n_unflat = tf.reshape(y_n, shape=[batch_size, num_timesteps, 2, 2])
+        note_gen_n_unflat = tf.reshape(note_gen_n, shape=[batch_size, num_timesteps, 2])
+        
+        #print('note_gen_n shape = ', note_gen_n.get_shape())
         
         #Append to notewise list
-        logP_out_list.append(logP_n_out_unflat)
-        pa_gen_out_list.append(pa_gen_n_unflat)
+        y_list.append(y_n_unflat)
+        note_gen_list.append(note_gen_n_unflat)
     
     # Convert output list to a Tensor
-    logP_out = tf.reshape(tf.stack(logP_out_list, axis=1), [batch_size, num_notes, num_timesteps, 2, 2])
-    pa_gen_out = tf.reshape(tf.stack(pa_gen_out_list, axis=1),  [batch_size, num_notes, num_timesteps, 2])
+    y_out = tf.reshape(tf.stack(y_list, axis=1), [batch_size, num_notes, num_timesteps, 2, 2])
+    note_gen_out = tf.reshape(tf.stack(note_gen_list, axis=1),  [batch_size, num_notes, num_timesteps, 2])
 
 
     
-    return logP_out, pa_gen_out
+    return y_out, note_gen_out
 
 
 
